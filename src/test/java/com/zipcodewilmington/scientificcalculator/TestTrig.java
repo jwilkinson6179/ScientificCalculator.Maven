b7e27ef67bbcae21d9cd9451dbe5c69c9884d257
Sandeep Naraya <snaraya@zipcodes-MacBook-Pro-9.local>
2020-02-09 16:45:41 -0500

    Added more test caese to TestTrig

--- src/test/java/com/zipcodewilmington/scientificcalculator/TestTrig.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/TestTrig.java
@@ -3,6 +3,8 @@ package com.zipcodewilmington.scientificcalculator;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class TestTrig {
 
